c453d12bbefa113727d25eb1793e5a3d412806d5
saffarini-dev-code <the.mscode@outlook.com>
2026-08-21 13:48:12 +0300

    feat: add EF Core application context

---
+++ src/POS_WPF/Data/AppDbContext.cs
@@ -0,0 +1,49 @@
+using Microsoft.EntityFrameworkCore;
+using POS_WPF.Domain.Products;
+using POS_WPF.Domain.Inventory;
+
+namespace POS_WPF.Data;
+
+public sealed class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<Product> Products => Set<Product>();
+    public DbSet<ProductUnit> ProductUnits => Set<ProductUnit>();
+    public DbSet<InventoryTransaction> InventoryTransactions => Set<InventoryTransaction>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(x => x.Id);
+            entity.Property(x => x.Name).HasMaxLength(200).IsRequired();
+            entity.Property(x => x.Sku).HasMaxLength(64).IsRequired();
+            entity.HasIndex(x => x.Sku).IsUnique();
+            entity.HasMany(x => x.Units)
+                .WithOne(x => x.Product)
+                .HasForeignKey(x => x.ProductId)
+                .OnDelete(DeleteBehavior.Restrict);
+        });
+
+        modelBuilder.Entity<ProductUnit>(entity =>
+        {
+            entity.HasKey(x => x.Id);
+            entity.Property(x => x.Name).HasMaxLength(100).IsRequired();
+            entity.Property(x => x.Abbreviation).HasMaxLength(20).IsRequired();
+            entity.Property(x => x.Barcode).HasMaxLength(100);
+            entity.Property(x => x.ConversionFactorToBase).HasPrecision(18, 6);
+            entity.Property(x => x.SellingPrice).HasPrecision(18, 2);
+            entity.Property(x => x.PurchasePrice).HasPrecision(18, 2);
+            entity.HasIndex(x => x.Barcode).IsUnique().HasFilter("[Barcode] IS NOT NULL");
+        });
+
+        modelBuilder.Entity<InventoryTransaction>(entity =>
+        {
+            entity.HasKey(x => x.Id);
+            entity.Property(x => x.TransactionQuantity).HasPrecision(18, 6);
+            entity.Property(x => x.ConversionFactor).HasPrecision(18, 6);
+            entity.Property(x => x.BaseQuantity).HasPrecision(18, 6);
+            entity.Property(x => x.Reference).HasMaxLength(100);
+            entity.HasIndex(x => new { x.ProductId, x.CreatedAt });
+        });
+    }
+}
