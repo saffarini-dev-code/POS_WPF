41d417d1bac9566bb9b48da933ce96ad20a1054d
saffarini-dev-code <the.mscode@outlook.com>
2026-08-21 14:16:59 +0300

    feat: wire dashboard navigation to POS window

--- src/POS_WPF/MainWindow.xaml.cs
+++ src/POS_WPF/MainWindow.xaml.cs
@@ -1,11 +1,11 @@
 using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace POS_WPF;
 
 public partial class MainWindow : Window
 {
-    public MainWindow()
-    {
-        InitializeComponent();
-    }
+    private readonly IServiceProvider _services;
+    public MainWindow(IServiceProvider services) { InitializeComponent(); _services = services; }
+    private void OpenPos_Click(object sender, RoutedEventArgs e) => _services.GetRequiredService<PosWindow>().Show();
 }
