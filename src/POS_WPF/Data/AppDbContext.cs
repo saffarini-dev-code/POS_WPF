80d9f7a97fd8da83e936546a1112507da002edc4
saffarini-dev-code <the.mscode@outlook.com>
2026-08-21 13:55:25 +0300

    feat: register security store sales and branch entities

--- src/POS_WPF/Data/AppDbContext.cs
+++ src/POS_WPF/Data/AppDbContext.cs
@@ -1,6 +1,10 @@
 using Microsoft.EntityFrameworkCore;
-using POS_WPF.Domain.Products;
+using POS_WPF.Domain.Customers;
 using POS_WPF.Domain.Inventory;
+using POS_WPF.Domain.Products;
+using POS_WPF.Domain.Security;
+using POS_WPF.Domain.Sales;
+using POS_WPF.Domain.Stores;
 
 namespace POS_WPF.Data;
 
@@ -9,6 +13,17 @@ public sealed class AppDbContext(DbContextOptions<AppDbContext> options) : DbCon
     public DbSet<Product> Products => Set<Product>();
     public DbSet<ProductUnit> ProductUnits => Set<ProductUnit>();
     public DbSet<InventoryTransaction> InventoryTransactions => Set<InventoryTransaction>();
+    public DbSet<User> Users => Set<User>();
+    public DbSet<Role> Roles => Set<Role>();
+    public DbSet<UserRole> UserRoles => Set<UserRole>();
+    public DbSet<RolePermission> RolePermissions => Set<RolePermission>();
+    public DbSet<Branch> Branches => Set<Branch>();
+    public DbSet<Warehouse> Warehouses => Set<Warehouse>();
+    public DbSet<Terminal> Terminals => Set<Terminal>();
+    public DbSet<CashRegister> CashRegisters => Set<CashRegister>();
+    public DbSet<Sale> Sales => Set<Sale>();
+    public DbSet<Customer> Customers => Set<Customer>();
+    public DbSet<Supplier> Suppliers => Set<Supplier>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -19,14 +34,8 @@ public sealed class AppDbContext(DbContextOptions<AppDbContext> options) : DbCon
             entity.Property(x => x.NameArabic).HasMaxLength(200);
             entity.Property(x => x.Sku).HasMaxLength(64).IsRequired();
             entity.HasIndex(x => x.Sku).IsUnique();
-            entity.HasOne<ProductUnit>()
-                .WithMany()
-                .HasForeignKey(x => x.BaseUnitId)
-                .OnDelete(DeleteBehavior.Restrict);
-            entity.HasMany(x => x.Units)
-                .WithOne(x => x.Product)
-                .HasForeignKey(x => x.ProductId)
-                .OnDelete(DeleteBehavior.Restrict);
+            entity.HasOne<ProductUnit>().WithMany().HasForeignKey(x => x.BaseUnitId).OnDelete(DeleteBehavior.Restrict);
+            entity.HasMany(x => x.Units).WithOne(x => x.Product).HasForeignKey(x => x.ProductId).OnDelete(DeleteBehavior.Restrict);
         });
 
         modelBuilder.Entity<ProductUnit>(entity =>
@@ -52,5 +61,40 @@ public sealed class AppDbContext(DbContextOptions<AppDbContext> options) : DbCon
             entity.HasIndex(x => new { x.ProductId, x.CreatedAt });
             entity.HasIndex(x => new { x.WarehouseId, x.ProductId, x.CreatedAt });
         });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(x => x.Id);
+            entity.Property(x => x.Username).HasMaxLength(100).IsRequired();
+            entity.Property(x => x.DisplayName).HasMaxLength(200).IsRequired();
+            entity.Property(x => x.PasswordHash).HasMaxLength(500).IsRequired();
+            entity.HasIndex(x => x.Username).IsUnique();
+        });
+        modelBuilder.Entity<Role>(entity => { entity.HasKey(x => x.Id); entity.Property(x => x.Name).HasMaxLength(100).IsRequired(); entity.HasIndex(x => x.Name).IsUnique(); });
+        modelBuilder.Entity<UserRole>(entity => { entity.HasKey(x => new { x.UserId, x.RoleId }); });
+        modelBuilder.Entity<RolePermission>(entity => { entity.HasKey(x => x.PermissionId); entity.Property(x => x.PermissionCode).HasMaxLength(150).IsRequired(); entity.HasIndex(x => new { x.RoleId, x.PermissionCode }).IsUnique(); });
+
+        modelBuilder.Entity<Branch>(entity => { entity.HasKey(x => x.Id); entity.Property(x => x.Code).HasMaxLength(50).IsRequired(); entity.Property(x => x.Name).HasMaxLength(200).IsRequired(); entity.HasIndex(x => x.Code).IsUnique(); });
+        modelBuilder.Entity<Warehouse>(entity => { entity.HasKey(x => x.Id); entity.Property(x => x.Code).HasMaxLength(50).IsRequired(); entity.Property(x => x.Name).HasMaxLength(200).IsRequired(); entity.HasIndex(x => new { x.BranchId, x.Code }).IsUnique(); });
+        modelBuilder.Entity<Terminal>(entity => { entity.HasKey(x => x.Id); entity.Property(x => x.Code).HasMaxLength(50).IsRequired(); entity.Property(x => x.Name).HasMaxLength(200).IsRequired(); entity.HasIndex(x => new { x.BranchId, x.Code }).IsUnique(); });
+        modelBuilder.Entity<CashRegister>(entity => { entity.HasKey(x => x.Id); entity.Property(x => x.Code).HasMaxLength(50).IsRequired(); entity.Property(x => x.Name).HasMaxLength(200).IsRequired(); entity.HasIndex(x => new { x.BranchId, x.Code }).IsUnique(); entity.Property(x => x.OpeningBalance).HasPrecision(18, 2); });
+
+        modelBuilder.Entity<Sale>(entity =>
+        {
+            entity.HasKey(x => x.Id);
+            entity.Property(x => x.Number).HasMaxLength(50).IsRequired();
+            entity.HasIndex(x => x.Number).IsUnique();
+            entity.Property(x => x.Subtotal).HasPrecision(18, 2);
+            entity.Property(x => x.Discount).HasPrecision(18, 2);
+            entity.Property(x => x.Tax).HasPrecision(18, 2);
+            entity.Property(x => x.Total).HasPrecision(18, 2);
+            entity.HasMany(x => x.Lines).WithOne().HasForeignKey("SaleId").OnDelete(DeleteBehavior.Cascade);
+            entity.HasMany(x => x.Payments).WithOne().HasForeignKey("SaleId").OnDelete(DeleteBehavior.Cascade);
+        });
+        modelBuilder.Entity<SaleLine>(entity => { entity.HasKey(x => x.Id); entity.Property(x => x.Description).HasMaxLength(250).IsRequired(); entity.Property(x => x.Quantity).HasPrecision(18, 6); entity.Property(x => x.UnitPrice).HasPrecision(18, 4); entity.Property(x => x.Discount).HasPrecision(18, 2); entity.Property(x => x.Tax).HasPrecision(18, 2); });
+        modelBuilder.Entity<SalePayment>(entity => { entity.HasKey(x => x.Id); entity.Property(x => x.Method).HasMaxLength(50).IsRequired(); entity.Property(x => x.Amount).HasPrecision(18, 2); });
+
+        modelBuilder.Entity<Customer>(entity => { entity.HasKey(x => x.Id); entity.Property(x => x.Code).HasMaxLength(50).IsRequired(); entity.Property(x => x.Name).HasMaxLength(200).IsRequired(); entity.Property(x => x.CreditLimit).HasPrecision(18, 2); entity.HasIndex(x => x.Code).IsUnique(); });
+        modelBuilder.Entity<Supplier>(entity => { entity.HasKey(x => x.Id); entity.Property(x => x.Code).HasMaxLength(50).IsRequired(); entity.Property(x => x.Name).HasMaxLength(200).IsRequired(); entity.HasIndex(x => x.Code).IsUnique(); });
     }
 }
